955dc06203b001e0a9c655bc9877e48e1e3c7bf2
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-03-25 17:38:15 -0300

    Criandos as paginas de login e regristro de ong

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi';
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+import heroes from '../../assets/heroes.png';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logo} alt="Heroes" />
+
+        <form>
+          <h1>Faça seu Login</h1>
+
+          <input placeholder="Informe seu ID" />
+          <button className="button" type="submit">Entrar</button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro.
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroes} alt="Heroes" />
+    </div>
+  );
+}
