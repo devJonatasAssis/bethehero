755dd688f51212c831352dbebad6e806cb06ffa3
Jonatas de Assis Silva <jonatasassis@MacBook-Pro-de-Jonatas.local>
2020-03-27 13:48:01 -0300

    Fazendo validação com Celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngController');
 const CasoController = require('./controllers/CasoController');
 const SessionController = require('./controllers/SessionController');
@@ -10,12 +10,31 @@ routes.post('/sessions/login', SessionController.store);
 
 // Rota da ONG
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        nome_ong: Joi.string().required(),
+        email_ong: Joi.string().required().email(),
+        whatsapp_ong: Joi.number().required().min(10).max(11),
+        cidade_ong: Joi.string().required(),
+        uf_ong: Joi.string().required().length(2)
+    })
+}), OngController.store);
 
 //Rota de Casos
 routes.get('/casos', CasoController.index);
-routes.get('/cases_details', CasoController.show);
+
+routes.get('/cases_details', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        ong_id: Joi.string().required()
+    }).unknown()
+}), CasoController.show);
+
 routes.post('/casos', CasoController.store);
-routes.delete('/casos/:id', CasoController.delete);
+
+routes.delete('/casos/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), CasoController.delete);
 
 module.exports = routes;
